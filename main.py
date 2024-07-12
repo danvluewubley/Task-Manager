945f2d05ad5eb72db4f59c18905901b8473b89b7
Daniel Wu <danielw72908@gmail.com>
2024-07-12 19:09:35 -0400

    integrated a base.html file

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# main cite
 @app.route('/')
 def index():
   return render_template('index.html')
 
+# localhost:5000/user/name
 @app.route('/user/<name>')
 def user(name):
   return render_template('user.html', name=name)
 
+# Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
   return render_template('404.html')
 
+# Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
   return render_template('500.html')
